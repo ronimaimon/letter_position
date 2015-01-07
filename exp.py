152ace7dbf54ba70bc33476894e023c8b8684637
ronimaimon <ronimaimon@gmail.com>
2015-01-07 21:39:58 +0200

    added fixation removed idea files from git

--- exp.py
+++ exp.py
@@ -8,7 +8,7 @@ If you publish work using this script please cite the relevant PsychoPy publicat
 """
 
 from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
-from psychopy import visual, core, data, event, logging, sound, gui
+from psychopy import visual, core, data, event, logging, gui
 from psychopy.constants import *  # things like STARTED, FINISHED
 import numpy as np  # whole numpy lib is available, prepend 'np.'
 from numpy import sin, cos, tan, log, log10, pi, average, sqrt, std, deg2rad, rad2deg, linspace, asarray
@@ -19,7 +19,6 @@ import os  # handy system and path functions
 CONFIGURATION_FILE = u'Scripts\\run%d.csv'
 END_TRIAL_DELAY = 20
 RESPONSE_KEY = 'b'
-BEGIN_TRIAL_DELAY = 2.0#24.0
 BLOCK_DURATION = 12.0
 IMAGE4_TIME = 11.0
 IMAGE3_TIME = 10.0
@@ -61,14 +60,24 @@ if(REFRESH_RATE==None):
     REFRESH_RATE = win.getActualFrameRate()
 expInfo['frameRate']= REFRESH_RATE#win.getActualFrameRate()
 
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+
 text = visual.TextStim(win=win, ori=0, name='text',
     text=u'The experiment will begin shortly',    font=u'Arial',
     pos=[0, 0], height=1, wrapWidth=None,
     color=u'white', colorSpace='rgb', opacity=1,
     depth=0.0)
-
-# Initialize components for Routine "trial"
-trialClock = core.Clock()
+fixation = visual.TextStim(win=win, ori=0, name='text',
+    text='+',    font=u'Arial',
+    pos=[0, 0], height=1, wrapWidth=None,
+    color=u'white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+visual.TextStim(win=win, ori=0, name='text',
+    text=u'The experiment will begin shortly',    font=u'Arial',
+    pos=[0, 0], height=1, wrapWidth=None,
+    color=u'white', colorSpace='rgb', opacity=1,
+    depth=0.0)
 image = visual.ImageStim(win=win, name='image',
     image='sin', mask=None,
     ori=0, pos=[0, 0], size=[8, 4],
@@ -110,39 +119,37 @@ keys = event.getKeys(keyList=['T','t','escape'])
 event.waitKeys(['return'])
 globalClock.reset()
 routineTimer.reset()
-routineTimer.add(BEGIN_TRIAL_DELAY)
+# routineTimer.add(BEGIN_TRIAL_DELAY)
 text.setAutoDraw(False)
 win.flip()
 if('escape' in keys):
     core.quit()
-while routineTimer.getTime() > 0:
-    continue
+# while routineTimer.getTime() > 0:
+#     continue
 for thisTrial in trials:
     trialComponents = []
-    trialComponents.append(ISI)
     trialComponents.append(image)
     trialComponents.append(image_2)
     trialComponents.append(image_3)
     trialComponents.append(image_4)
-    trialComponents.append(memory_inst)
     for thisComponent in trialComponents:
         if hasattr(thisComponent, 'status'):
             thisComponent.status = NOT_STARTED
 
     t = 0
     trialClock.reset()  # clock
-    frameN = -1
     routineTimer.add(BLOCK_DURATION)
     #-------Start Routine "trial"-------
     continueRoutine = True
+    isFirstFrame = True
     while continueRoutine and routineTimer.getTime() > 0:
         # get current time
         t = trialClock.getTime()
-        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
         # *ISI* period
-        if t >= 0.0 and ISI.status == NOT_STARTED:
+        if isFirstFrame and t >= 0.0:
             thisExp.addData("start",globalClock.getTime())
-            ISI.status = STARTED
+            fixation.setAutoDraw(True)
+            isFirstFrame = False
             if thisTrial != None:
                 for paramName in thisTrial.keys():
                     exec(paramName + '= thisTrial.' + paramName)
@@ -162,7 +169,7 @@ for thisTrial in trials:
             thisExp.addData("start stim",globalClock.getTime())
             # keep track of start time/frame for later
             image.tStart = t  # underestimates by a little under one frame
-            image.frameNStart = frameN  # exact frame index
+            fixation.setAutoDraw(False)
             image.setAutoDraw(True)
         elif image.status == STARTED and t >= (IMAGE1_TIME + (IMAGE_DURATION -win.monitorFramePeriod*0.75)): #most of one frame period left
             image.setAutoDraw(False)
@@ -171,7 +178,6 @@ for thisTrial in trials:
         elif t >= IMAGE2_TIME and image_2.status == NOT_STARTED:
             # keep track of start time/frame for later
             image_2.tStart = t  # underestimates by a little under one frame
-            image_2.frameNStart = frameN  # exact frame index
             image_2.setAutoDraw(True)
         elif image_2.status == STARTED and t >= (IMAGE2_TIME + (IMAGE_DURATION -win.monitorFramePeriod*0.75)): #most of one frame period left
             image_2.setAutoDraw(False)
@@ -180,7 +186,6 @@ for thisTrial in trials:
         elif t >= IMAGE3_TIME and image_3.status == NOT_STARTED:
             # keep track of start time/frame for later
             image_3.tStart = t  # underestimates by a little under one frame
-            image_3.frameNStart = frameN  # exact frame index
             image_3.setAutoDraw(True)
         elif image_3.status == STARTED and t >= (IMAGE3_TIME + (IMAGE_DURATION -win.monitorFramePeriod*0.75)): #most of one frame period left
             image_3.setAutoDraw(False)
@@ -189,7 +194,6 @@ for thisTrial in trials:
         elif t >= IMAGE4_TIME and image_4.status == NOT_STARTED:
             # keep track of start time/frame for later
             image_4.tStart = t  # underestimates by a little under one frame
-            image_4.frameNStart = frameN  # exact frame index
             image_4.setAutoDraw(True)
         elif image_4.status == STARTED and t >= (IMAGE4_TIME + (IMAGE_DURATION -win.monitorFramePeriod*0.75)): #most of one frame period left
             image_4.setAutoDraw(False)
@@ -197,14 +201,9 @@ for thisTrial in trials:
         if not continueRoutine:  # a component has requested a forced-end of Routine
             routineTimer.reset()  # if we abort early the non-slip timer needs reset
             break
-        continueRoutine = False  # will revert to True if at least one component still running
-        for thisComponent in trialComponents:
-            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                continueRoutine = True
-                break  # at least one component has not yet finished
 
         if event.getKeys(keyList=[RESPONSE_KEY]):
-            thisExp.addData("keyPressed","b")
+            thisExp.addData("keyPressed",RESPONSE_KEY)
             thisExp.addData("keyRT",trialClock.getTime())
         # check for quit (the Esc key)
         if event.getKeys(keyList=["escape"]):
