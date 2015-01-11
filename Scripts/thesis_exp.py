e5e5c8991e553a3fe4300b6fc08173160bb7d726
ronimaimon <ronimaimon@gmail.com>
2015-01-11 11:31:22 +0200

    add catch trials to run generator

--- Scripts/thesis_exp.py
+++ Scripts/thesis_exp.py
@@ -42,37 +42,38 @@ class Triplets:
                 for i in range(len(types)):
                     runs[i].append([os.path.join(self.root[3:],name,'base'+'.bmp'),(os.path.join(self.root[3:],name,types[i]+'.bmp')),'',''])
 
-print os.path.dirname(__file__)
-#t = Triplets(os.path.join('..','stimuli','Triplets'))
-z2 = PWBlocks(os.path.join('..','stimuli','Z2'),os.path.join('..','stimuli','Z3'),4)
-#z3 = PWBlocks(os.path.join('..','stimuli','Z3'),os.path.join('..','stimuli','Z3'),4)
-runs = [[],[]]
-#t.splitToRuns(runs)
-z2.splitToRuns(runs)
-#z3.splitToRuns(runs)
-for run in runs:
-    random.shuffle(run)
-    for i in range(len(run)/5):
-        word = ''
-        while word=='':
-            flat_run =  sum(run,[])
-            index = random.randrange(len(flat_run))
-            word = flat_run[index]
-        index = (5*(i+1))-random.randrange(3)+i
-        print index
-        run.insert(index,[word,'','',''])
-        # run.insert((5*(i+1))-random.randrange(3)+2,[word,None,None,None])
-for i in range(len(runs)):
-    file = open('run'+str(i+1)+'.csv', 'wb')
-    header = ['stim1','stim2','stim3','stim4']
-    wr = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
-    wr.writerow(header)
-    wr.writerows(runs[i])
-    print runs[i]
-    # win=visual.Window(fullscr=True)
-    # im1 = SimpleImageStim(win, image=os.path.join('..','stimuli', '1.bmp'), units='', pos=(0.0, 0.0), flipHoriz=False, flipVert=False, name='PW1',
-    #                 autoLog=True)
-    # im1.draw()
-    # win.flip()
+def generateMVPARuns():
+    global z2, runs, run, i, index, catch_trial, flat_run, word, word2, word3, loc, file, header, wr
+    z2 = PWBlocks(os.path.join('..', 'stimuli', 'Z2'), os.path.join('..', 'stimuli', 'Z3'), 4)
+    runs = [[], []]
+    z2.splitToRuns(runs)
+    for run in runs:
+        random.shuffle(run)
+        for i in range(len(run) / 5):
+            index = (5 * (i + 1)) - random.randrange(3) + i
+            catch_trial = []
+            flat_run = sum(run[:index], [])
+            word = flat_run[random.randrange(len(flat_run))]
+            word2 = flat_run[random.randrange(len(flat_run))]
+            word3 = flat_run[random.randrange(len(flat_run))]
+            loc = random.randrange(3)
+            if (loc == 0):
+                catch_trial = [word, word, word2, word3]
+            elif (loc == 1):
+                catch_trial = [word2, word, word, word3]
+            else:
+                catch_trial = [word2, word3, word, word]
+            print index
+            run.insert(index, catch_trial)
+    for i in range(len(runs)):
+        file = open('exp1-run' + str(i + 1) + '.csv', 'wb')
+        header = ['stim1', 'stim2', 'stim3', 'stim4']
+        wr = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+        wr.writerow(header)
+        wr.writerows(runs[i])
+        print runs[i]
 
-    # core.wait(8.0)
+print os.path.dirname(__file__)
+generateMVPARuns()
+t = Triplets(os.path.join('..','stimuli','Triplets'))
+t.splitToRuns(runs)
