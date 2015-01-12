22e29ba671b887fd58f8be80ad3d9fb27969dd3f
ronimaimon <ronimaimon@gmail.com>
2015-01-12 17:21:38 +0200

    generate triplets and new catch trials

--- Scripts/thesis_exp.py
+++ Scripts/thesis_exp.py
@@ -34,13 +34,17 @@ class Triplets:
     def __init__(self, path):
         self.root = path
     def splitToRuns(self,runs=[]):
-        types = ['same','subs','trans']
+        types = ['base','sub','trans']
+        all_blocks = []
         for root, dirs, files in os.walk(self.root, topdown=False):
             for name in dirs:
                 random.shuffle(types)
                 path = os.path.join(root, name)
                 for i in range(len(types)):
-                    runs[i].append([os.path.join(self.root[3:],name,'base'+'.bmp'),(os.path.join(self.root[3:],name,types[i]+'.bmp')),'',''])
+                    all_blocks.append([os.path.join(self.root[3:],name,'base'+'.png'),(os.path.join(self.root[3:],name,types[i]+'.png'))])
+        random.shuffle(all_blocks)
+        runs[0] = runs[0]+ all_blocks[:len(all_blocks)/2]
+        runs[1] = runs[1]+ all_blocks[len(all_blocks)/2:]
 
 class LocalizerBlocks:
     def __init__(self, path,second_path,block_len):
@@ -71,24 +75,21 @@ def generateMVPARuns():
     z2 = PWBlocks(os.path.join('..', 'stimuli', 'Z2'), os.path.join('..', 'stimuli', 'Z3'), 4)
     runs = [[], []]
     z2.splitToRuns(runs)
+    catch_trials = os.listdir(os.path.join('..', 'stimuli', 'Catch'))
+    catch_trials = map(lambda file: os.path.join('stimuli', 'Catch',file),catch_trials)
+    random.shuffle(catch_trials)
     for run in runs:
         random.shuffle(run)
         for i in range(len(run) / 5):
             index = (5 * (i + 1)) - random.randrange(3) + i
-            catch_trial = []
             flat_run = sum(run[:index], [])
             word = flat_run[random.randrange(len(flat_run))]
             word2 = flat_run[random.randrange(len(flat_run))]
             word3 = flat_run[random.randrange(len(flat_run))]
-            loc = random.randrange(3)
-            if (loc == 0):
-                catch_trial = [word, word, word2, word3]
-            elif (loc == 1):
-                catch_trial = [word2, word, word, word3]
-            else:
-                catch_trial = [word2, word3, word, word]
+            catch_block = [word, word2, word3]
+            catch_block.insert(random.randrange(4),catch_trials[i])
             print index
-            run.insert(index, catch_trial)
+            run.insert(index, catch_block)
     for i in range(len(runs)):
         file = open('exp1-run' + str(i + 1) + '.csv', 'wb')
         header = ['stim1', 'stim2', 'stim3', 'stim4']
@@ -109,9 +110,35 @@ def generateLocalizerRun():
     wr.writerow(header)
     wr.writerows(run)
 
+def generateTriplets():
+    runs = [[], []]
+    t = Triplets(os.path.join('..','stimuli','Triplets'))
+    t.splitToRuns(runs)
+    catch_trials = os.listdir(os.path.join('..', 'stimuli', 'Catch'))
+    catch_trials = map(lambda file: os.path.join('stimuli', 'Catch',file),catch_trials)
+    random.shuffle(catch_trials)
+    for run in runs:
+        random.shuffle(run)
+        for i in range(len(run) / 14):
+            index = (14 * (i + 1)) - random.randrange(10) + i
+            flat_run = sum(run[:index], [])
+            word = flat_run[random.randrange(len(flat_run))]
+            catch_block = [word]
+            catch_block.insert(random.randrange(2),catch_trials[i])
+            print index
+            run.insert(index, catch_block)
+    for i in range(len(runs)):
+        file = open('exp2-run' + str(i + 1) + '.csv', 'wb')
+        header = ['stim1', 'stim2']
+        wr = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+        wr.writerow(header)
+        wr.writerows(runs[i])
+        print runs[i]
+
 
 
 print os.path.dirname(__file__)
+generateTriplets()
 generateMVPARuns()
 generateLocalizerRun()
 # t = Triplets(os.path.join('..','stimuli','Triplets'))
